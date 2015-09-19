e0107b3a85d4b2dd40b835d32749f53bbc3738b9
Casey Marshall <cmars@cmarstech.com>
2015-09-19 17:54:23 -0500

    Use postgres storage for running from command-line.

--- cmd/oostore/main.go
+++ cmd/oostore/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"gopkg.in/errgo.v1"
 
 	"github.com/cmars/oostore"
+	"github.com/cmars/oostore/postgres"
 )
 
 const (
@@ -18,7 +21,7 @@ const (
 func main() {
 	app := cli.NewApp()
 	app.Name = "oostore"
-	app.Usage = "opaque object storage service"
+	app.Usage = "oostore [--addr ADDR] [--prefix PREFIX] [database connect string]"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "addr",
@@ -29,10 +32,25 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) {
-		// TODO: persistent object & macaroon storage
-		store := oostore.NewMemStorage()
+		dsn := "host=/var/run/postgresql database=oostore"
+		if c.Args().Present() {
+			dsn = strings.Join(c.Args(), " ")
+		}
+		db, err := sql.Open("postgres", dsn)
+		if err != nil {
+			log.Fatalf("cannot connect to database: %s", errgo.Details(err))
+		}
+		objectStore, err := postgres.NewObjectStorage(db)
+		if err != nil {
+			log.Fatalf("failed to instantiate object storage: %s", errgo.Details(err))
+		}
+		bakeryStore, err := postgres.NewBakeryStorage(db)
+		if err != nil {
+			log.Fatalf("failed to instantiate bakery storage: %s", errgo.Details(err))
+		}
 		service, err := oostore.NewService(oostore.ServiceConfig{
-			ObjectStore: store,
+			ObjectStore: objectStore,
+			BakeryStore: bakeryStore,
 			Prefix:      c.String("prefix"),
 		})
 		if err != nil {
